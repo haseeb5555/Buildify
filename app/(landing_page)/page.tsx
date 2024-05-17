d0315932686fd3b98c5cf53ed346090cbda3cd2c
MuhammadHaseeb12 <sp21-bse-027@cuilahore.edu.pk>
2024-05-17 21:15:44 +0500

    fix: landing page error

--- app/(landing_page)/page.tsx
+++ app/(landing_page)/page.tsx
@@ -2,16 +2,10 @@ import CustomerReviews from '@/components/CustomerReviews'
 import { Hero } from '@/components/Hero'
 import Services from '@/components/Services'
 import Why from '@/components/why-to-choose-us'
-import { currentUser } from '@clerk/nextjs'
-import { redirect } from 'next/navigation'
 
 
 
 export default async function Home() {
-  const user =await currentUser()
-  if (user){
-    redirect("")
-  }
 
   return (
     <>
